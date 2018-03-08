faf910a4ce83aaef25b3fd1f07b83733981ed831
hlc <hlc19890519@126.com>
2018-03-08 15:03:42 +0800

    xbrl-platform schedulejob、common项目新增

---
+++ modules/xbrl-schedulejob/src/main/java/com/changhong/xbrl/schedulejob/XbrlSchedulejobApplication.java
@@ -0,0 +1,12 @@
+package com.changhong.xbrl.schedulejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrlSchedulejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrlSchedulejobApplication.class, args);
+	}
+}
---
+++ xbrl-platform-common/src/main/java/com/changhong/xbrl/common/XbrlPlatformCommonApplication.java
@@ -0,0 +1,12 @@
+package com.changhong.xbrl.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrlPlatformCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrlPlatformCommonApplication.class, args);
+	}
+}
